45df5a215db61312822fe7c3e4570d8412ded07b
Ramon S <againretried@gmail.com>
2024-05-04 12:02:00 +0200

    Change Card to a record-type

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,10 +1,4 @@
-
-
-public class Card {
-    private final String name;
-    private final int[] numberNeedToHarvest;
-    private final int[] coins;
-
+public record  Card(String name, int[] numberNeedToHarvest, int[] coins) {
     public Card(String name, int[] numberNeedToHarvest, int[] coins) {
         this.name = name;
         this.numberNeedToHarvest = numberNeedToHarvest;
@@ -12,17 +6,5 @@ public class Card {
         if (numberNeedToHarvest.length != coins.length) {
             throw new IllegalArgumentException();
         }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int[] getNumberNeedToHarvest() {
-        return numberNeedToHarvest;
-    }
-
-    public int[] getCoins() {
-        return coins;
     }
 }
