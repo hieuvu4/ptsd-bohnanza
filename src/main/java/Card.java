a8a70c5aaea3f2d31d49111d5769634bc38b4917
Ramon S <againretried@gmail.com>
2024-05-04 12:05:27 +0200

    Revert "Change Card to a record-type"
    This reverts commit 45df5a215db61312822fe7c3e4570d8412ded07b.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,4 +1,10 @@
-public record  Card(String name, int[] numberNeedToHarvest, int[] coins) {
+
+
+public class Card {
+    private final String name;
+    private final int[] numberNeedToHarvest;
+    private final int[] coins;
+
     public Card(String name, int[] numberNeedToHarvest, int[] coins) {
         this.name = name;
         this.numberNeedToHarvest = numberNeedToHarvest;
@@ -6,5 +12,17 @@ public record  Card(String name, int[] numberNeedToHarvest, int[] coins) {
         if (numberNeedToHarvest.length != coins.length) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getNumberNeedToHarvest() {
+        return numberNeedToHarvest;
+    }
+
+    public int[] getCoins() {
+        return coins;
     }
 }
